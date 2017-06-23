d5a282a73dfcfc16d691177c4eb41d551e9ed52d
ホアンゴックシン <hoangngocsinh@sinhhn@brave.local>
2017-06-23 18:21:07 +0900

    xsds

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Hi Jenkins! how about you !!!';
+  title = 'Hi Jenkins! how about you ...!!!';
 }
