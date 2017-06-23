017a938007851af1cacdf66387b82b9b786d26a4
ホアンゴックシン <hoangngocsinh@sinhhn@brave.local>
2017-06-23 18:00:40 +0900

    heelo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Bye Jenkins World... !!!';
+  title = 'Hello Jenkins World... !!!';
 }
